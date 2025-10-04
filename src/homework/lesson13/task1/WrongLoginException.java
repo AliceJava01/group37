67b1903801c41b86f4f9c120df12fde53cf102b2
Alice <gusarowaalice@yandex.ru>
2025-10-04 20:37:10 +0300

    Add a folders with class
    - work with try/catch/finally
    - completed tasks 1,*

---
+++ src/homework/lesson13/task1/WrongLoginException.java
@@ -0,0 +1,11 @@
+package homework.lesson13.task1;
+
+public class WrongLoginException extends RuntimeException {
+    public WrongLoginException() {
+
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
